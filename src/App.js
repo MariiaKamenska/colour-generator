014f7fa5c3852c21422c1bfd1b7a342d14614420
MariiaKamenska <kamenskaam53@gmail.com>
2025-01-04 20:02:28 +0200

    first version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
+import React, {useState} from "react";
 
 function App() {
-  return <div></div>;
+  const [colour, setColour] = useState('#fff')
+
+  const generateColour = () => {
+    const randomColour = `#${Math.floor(Math.random() * 16999999).toString(16)}`
+    setColour(randomColour);
+  };
+  return (
+    <div style={{...styles.container, backgroundColor: colour}}>
+      <h1>Colour generator</h1>
+      <p style={styles.p}>The colour is {colour}</p>
+      <button onClick={generateColour} style={styles.button}>Generate</button>
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  p: {
+    color: '#696969',
+  },
+  button: {
+    backgroundColor: '#288BA8',
+  }
 }
 
 export default App;
