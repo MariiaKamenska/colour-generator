31814642f24313da103b1d9823f255edd5732980
MariiaKamenska <kamenskaam53@gmail.com>
2025-01-25 18:35:28 +0200

    changed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 function App() {
-  const [colour, setColour] = useState('#fff')
+  const [colour, setColour] = useState('#fff');
+
+  const [colorList, setColorList] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:5000/colors').then((response) => setColorList(response.data)).catch((error) => console.error('Помилка з завантаженням кольорів:', error));
+  }, [])
 
   const generateColour = () => {
       const randomColour = `#${Math.floor(Math.random() * 16999999).toString(16)}`
     setColour(randomColour);
+    axios.get('http://localhost:5000/colors', { color: randomColour })
+    // .then((response) => setColorList( -))
+    .catch((error) => console.error('Помилка з додаванням кольору:', error));
+
   };
+
   return (
     <div style={{...styles.container, backgroundColor: colour}}>
       <h1>Colour generator</h1>
       <p style={styles.p}>The colour is {colour}</p>
       <button onClick={generateColour} style={styles.button}>Generate</button>
-      {/* <div class="rectangle" style>ee</div> */}
+      
+      <h2></h2>
+      <ul>
+
+      </ul>
     </div>
   
   );
